198cb73bb06c1e65d1e6fc65b6f9b446541e4d0a
Antoine <fevre.pasquet@gmail.com>
2023-06-10 17:40:18 +0200

    UPDATE bot

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+/* --->>> Replace <your_discord_id> with your Discord ID <<<--- */
 client.on('messageCreate', message => {
-    if (message.author.id == '<your_discord_id>') {
+    if (message.author.id == '<your_discord_id>') { 
         client.channels.fetch(message.channelId)
             .then(channel => {
                 channel.send(message.content);
-            }
-    );
+            });
     }
 });
 
+/* --->>> Replace <your_bot_token> with your bot token available here: https://discord.com/developers/applications/<your_bot_token>/bot <<<--- */
 client.login("<your_bot_token>");
